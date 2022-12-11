bde2f36de7676fa2b2c42147909f3c3f7ffb7f54
Pavel Shcherbatyuk <119248373+tsherbatyuk-pa@users.noreply.github.com>
2022-12-11 23:17:58 +0300

    Update task5.py

--- Лабораторная работа 5/task5.py
+++ Лабораторная работа 5/task5.py
@@ -1,10 +1,10 @@
 from string import ascii_lowercase, ascii_uppercase, digits  # Импортируем указанные библиотеки
-import random  # импортируем модуль рандом для дальнейшей генерации чисел
+from random import sample  # импортируем функцию из модуля рандом для дальнейшей генерации чисел
 
 
 def get_random_password(len_) -> str:  # аргумент -- длина пароля
     population_ = ascii_uppercase + ascii_lowercase + digits  # последовательность допуст. символов
-    list_ = random.sample(population_, len_)  # генерация пароля по указанной последовательности и длине
+    list_ = sample(population_, len_)  # генерация пароля по указанной последовательности и длине
     return ''.join(list_)  # обьединяем элементы списка без разделителя
 
 
